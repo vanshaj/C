31f67c51f17fcdf0f63ab64486787abdbd8adb9e
vanshaj <vanshajdhar@gmail.com>
2023-04-28 03:26:44 +0000

    Added part 1 of chapter2 1. Pointer 2. Dereference

--- headFirst/chapter1/func.c
+++ headFirst/chapter1/func.c
@@ -19,6 +19,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int z = 3; // will get stored in the globals section of the memory
 int larger(int x, int y){
 	if (x>y){
 		return x;
@@ -27,7 +28,7 @@ int larger(int x, int y){
 }
 
 int main(){
-	int max;
+	int max; // will get stored on the stack
 	max = larger(100, 101);
 	printf("max value is %d\n ", max);
 	return 0;
---
+++ headFirst/chapter2/memory.c
@@ -0,0 +1,49 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  memory.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/28/23 02:54:12
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+void go_south_east(int *lat, int *lon){
+	puts("Going south east");
+	printf("Address of lat is %p\n", lat);
+	*lat = *lat -1;
+	*lon = *lon +1;
+	// Defining memory
+	int y = 3;
+	int *memy = &y;
+	int valY = *memy;
+	printf("Address of y is %p\n", memy);
+	printf("Value of y is %d\n", valY);
+
+}
+
+int main(){
+	int x = 4;
+	printf("x is stored at %p\n", &x);
+
+	int lat = 53;
+	int lon = 33;
+	printf("lat is now %d, lon is now %d\n", lat, lon);
+	printf("Address of lat is %p\n", &lat);
+	go_south_east(&lat, &lon);
+	printf("lat is now %d, lon is now %d\n", lat, lon);
+	return 0;
+}
+
+
+
