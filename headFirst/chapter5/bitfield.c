f934d663d6e30a9387df3da2f8313b484dccafc9
vanshaj <vanshajdhar@gmail.com>
2023-06-09 15:46:10 +0000

    Added  malloc

---
+++ headFirst/chapter5/bitfield.c
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  bitfield.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  06/09/23 14:58:18
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
+typedef struct bitfield{
+	unsigned int low_pass_vcf:1;
+	unsigned int filter_compiler:1;
+}bitf;
+
+int main(){
+
+}
